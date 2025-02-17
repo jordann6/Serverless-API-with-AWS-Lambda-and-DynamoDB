c539cf7ca51dbb9984b5d687d1dba131416cab88
Jordan <JordanDiNelson@gmail.com>
2025-02-17 17:04:13 -0600

    variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "region" {
   description = "The AWS region where resources will be created"
   type        = string
-  default     = "us-east-1"  # Default region (you can override this when applying)
+  default     = "us-east-1" 
 }
